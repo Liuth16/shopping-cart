2553c1796a59c8b8c1867035099879b57a392a1e
Leandro <63678585+Liuth16@users.noreply.github.com>
2025-05-29 15:11:34 -0300

    Add toast bar effect

--- src/components/Toast.jsx
+++ src/components/Toast.jsx
@@ -1,10 +1,12 @@
-import styles from "../styles/toast.module.css"
+import styles from "../styles/toast.module.css";
 
-export const Toast = ({message, show}) => {
-    return (
-        <div className={`${styles.toast} ${show ? styles.show : ""}`} >
-            <div className={styles.greenBar} ></div>
-            <span> {message} </span>
-        </div>
-    )
-}
+export const Toast = ({ message, show }) => {
+  return (
+    <div className={`${styles.toast} ${show ? styles.show : ""}`}>
+      <div className={styles.progressBarContainer}>
+        <div className={`${styles.progressBar} ${show ? styles.animate : ""}`}></div>
+      </div>
+      <span>{message}</span>
+    </div>
+  );
+};
